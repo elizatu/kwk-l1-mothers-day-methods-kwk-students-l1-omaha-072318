bd2c0d0e07a9ece549ada9f31eb9c6e4e07a7e93
elizatu <eliza.turco@gmail.com>
2018-07-30 15:09:08 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,5 +1,5 @@
 def holiday_greeting(holiday = "Mother's Day", to = "Mom", from = "Your Favorite Child")
   puts "Happy #{holiday}, #{to}! From #{from}"
 end
-holiday_greeting("Easter", "Alfred", "ET")
+
 
